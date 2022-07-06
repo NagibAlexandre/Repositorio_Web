3f869427b4f3c906e242d39f677f357b553f634d
Nagib Alexandre <nagibverly@gmail.com>
2022-07-06 11:54:29 -0300

    aula web nagib

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+jogo1 = Jogo('God of War', 'Ação', 'Playstation')
+jogo2 = Jogo('Cs:Go', 'Tiro', 'PC')
+jogo3 = Jogo('Minecraft', 'Construção', 'PC')
+
+ListaJ = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__)
+
+
+@app.route('/inicio')
+def ola():
+    return render_template('lista.html',titulo='Meus Jogos', jogos=ListaJ)
+
+
+app.run()
